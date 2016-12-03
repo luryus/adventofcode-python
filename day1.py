3b09d09d5b05eed2c61c3d374cc4f9bc9bcbb3da
Lauri Koskela <lapamiko@gmail.com>
2016-12-03 09:52:46 +0200

    Day 1, Day 2

---
+++ day1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import sys
+
+DIR_NORTH = 0
+DIR_EAST = 1
+DIR_SOUTH = 2
+DIR_WEST = 3
+
+def main():
+    if len(sys.argv) != 2:
+        print("Invalid parameters. Give input file name.")
+        return
+
+    inputfile = sys.argv[1]
+
+    inputtext = ""
+    with open(inputfile, 'r') as f:
+        inputtext = f.read()
+
+    if not inputtext:
+        print("Invalid input file")
+        return
+
+    commands = inputtext.strip().split(', ')
+
+    x = 0
+    y = 0
+    direction = DIR_NORTH
+
+    for c in commands:
+        if c[0] == 'L':
+            direction = (direction - 1) % 4
+        else:
+            direction = (direction + 1) % 4
+
+        distance = int(c[1:])
+        if direction == DIR_NORTH:
+            y -= distance
+            print("Moving", distance, "north")
+        elif direction == DIR_WEST:
+            x -= distance
+            print("Moving", distance, "west")
+        elif direction == DIR_SOUTH:
+            y += distance
+            print("Moving", distance, "south")
+        elif direction == DIR_EAST:
+            x += distance
+            print("Moving", distance, "east")
+
+    print("x: {0}, y: {1}, distance: {2}".format(x, y, abs(x) + abs(y)))
+
+
+
+main()
---
+++ day2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import sys
+
+def move_up(digit: int) -> int:
+    if (digit > 3):
+        digit -= 3
+    return digit
+
+
+def move_down(digit: int) -> int:
+    if (digit < 7):
+        digit += 3
+    return digit
+
+
+def move_left(digit: int) -> int:
+    if (digit not in [1, 4, 7]):
+        digit -= 1
+    return digit
+
+
+def move_right(digit: int) -> int:
+    if (digit not in [3, 6, 9]):
+        digit += 1
+    return digit
+
+
+def get_code_digit(line: str) -> int:
+    digit = 5
+    for direction in line.strip():
+        print('Moving to direction', direction)
+        if direction == 'U':
+            digit = move_up(digit)
+        elif direction == 'D':
+            digit = move_down(digit)
+        elif direction == 'L':
+            digit = move_left(digit)
+        elif direction == 'R':
+            digit = move_right(digit)
+        else:
+            raise RuntimeError('Invalid direction ' + direction)
+
+    return digit
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Invalid parameters. Give input file name.")
+        return
+
+    inputfile = sys.argv[1]
+
+    code = []
+
+    with open(inputfile, 'r') as f:
+        for line in f:
+            digit = get_code_digit(line)
+            code.append(digit)
+
+    print('Code:', ''.join(str(d) for d in code))
+
+
+main()
