4928d9bd1eec94cd19de1db8fce714f729872c85
Lauri Koskela <lapamiko@gmail.com>
2016-12-06 19:35:26 +0200

    Day 6

---
+++ day6-1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import Counter
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Invalid parameters. Give input file name.")
+        return
+
+    inputfile = sys.argv[1]
+    columns = []
+
+    with open(inputfile) as f:
+        for line in f:
+            for i in range(len(line.strip())):
+                if len(columns) <= i:
+                    columns.append(Counter([line[i]]))
+                else:
+                    columns[i].update([line[i]])
+
+    print('Message', ''.join([c.most_common()[0][0] for c in columns]))
+
+
+main()
